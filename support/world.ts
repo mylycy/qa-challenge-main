09ef6f217d0222eac4216771a1e10e84d919e791
Mylycy <mylycybellini@gmail.com>
2025-05-27 20:13:40 -0300

    updates ci/cd one more

--- support/world.ts
+++ support/world.ts
@@ -1,6 +1,6 @@
 // support/world.ts
 import { setWorldConstructor, World, IWorldOptions } from '@cucumber/cucumber';
-import { Page } from 'playwright';
+import { Page } from '@playwright/test'; // ✅ certo
 
 export class CustomWorld extends World {
   page!: Page; // será inicializada nos hooks
