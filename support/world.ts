a1b8183fea0c10bf7d2ffa6c6d086d1688fc5fbb
Mylycy <mylycybellini@gmail.com>
2025-05-27 23:32:12 -0300

    updates two more

--- support/world.ts
+++ support/world.ts
@@ -1,8 +1,9 @@
 // support/world.ts
 import { setWorldConstructor, World, IWorldOptions } from '@cucumber/cucumber';
 import { Page } from '@playwright/test'; 
+
 export class CustomWorld extends World {
-  page!: Page; // será inicializada nos hooks
+  page!: Page; 
 
   constructor(options: IWorldOptions) {
     
